ba05d246e88586ab66234836974a4551518d642f
ebrayt01 <eric@Erics-MacBook-Pro.local>
2014-09-07 12:39:52 -0700

    Backed out changed with merge conflict for Members

--- client/views/members/members.js
+++ client/views/members/members.js
@@ -4,31 +4,6 @@ Template.members.rendered = function() {
   document.title = 'Members | Meteor Vegas';
   $('<meta>', { name: 'description', content: 'A Bi-Monthly Meteor.js Meetup in Las Vegas, NV' }).appendTo('head');
 };
-<<<<<<< HEAD
-=======
-
-
-Template.members.helpers({
-  post: function() {
-    //console.log("Post");
-	var posts = [];
-    var _i = 0;
-    Meteor.users.find({}, {sort: ["profile.name", "asc"]}).forEach(function(p) {
-      p.position = _i;
-      _i++;
-      posts.push(p);
-    });
-    return posts;
-  },
-  odd: function() {
-	  //console.log("Odd");
-	return !(this.position % 2 === 0);
-  },
-  even: function() {
-	  //console.log("Even");
-    return (this.position % 2 === 0);
-  }
-});
 
 Template.members.events({
     'click .createMembers': function () {
@@ -72,12 +47,7 @@ Template.members.events({
 
 Template.members.members = function () {
 
-//results = Meteor.users.find({});
-
-  return Meteor.users.find({});
+  return Meteor.users.find({},{sort: ["profile.name", "asc"]});
 	
 };
 
-
-
->>>>>>> members
