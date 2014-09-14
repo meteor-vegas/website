77d9b91a463474bf77d636778c2247d38cd1ae2e
Abdul Zalil <abdul.zalil@gmail.com>
2014-09-14 22:21:42 +0000

    add presentation feature

--- client/views/presentations/presentations.js
+++ client/views/presentations/presentations.js
@@ -1,3 +1,7 @@
+//Global
+var presObj = {};
+var step = 1;
+
 Template.presentations.rendered = function() {
 };
 
@@ -17,38 +21,43 @@ Template.presentations.events({
     $("#presentation-wrapper").hide();
     $("input#url").val("");
     $("#btn-add-presentation").text("Next");
+    step = 1;
   },  
 
   'submit': function(event, template) {
     event.preventDefault();
-    Session.set("presentationURL", $("input#url").val());
-    
-    console.log("submitting");
-    $("#url-wrapper").slideUp();
-    $("#presentation-wrapper").slideDown();
-    Meteor.setTimeout(function(){
-       $("a#presentation-embed").oembed(null, {
-          embedMethod:"fill",
-          afterEmbed: function(data) {
-            console.log(data);
-            $("#btn-add-presentation").text("Add");
-            if (Meteor.user()) {
-                var presObj = {};
-                if(data.title) {
-                    presObj.title = data.title;
-                }
-                if(data.thumbnail_url) {
-                    presObj.thumbnail_url = data.thumbnail_url;
-                }
-                presObj.oembed = data;
-                var newPresentationID = Presentations.insert(presObj);
-                if (newPresentationID) {
-                    Router.go("presentation",{_id: newPresentationID});
+    if(step===1) {
+        Session.set("presentationURL", $("input#url").val());
+        
+        console.log("submitting");
+        $("#url-wrapper").slideUp();
+        $("#presentation-wrapper").slideDown();
+        step=2;
+        Meteor.setTimeout(function(){
+           $("a#presentation-embed").oembed(null, {
+              embedMethod:"fill",
+              afterEmbed: function(data) {
+                console.log(data);
+                $("#btn-add-presentation").text("Add");
+                if (Meteor.user()) {
+                    presObj={};
+                    if(data.title) {
+                        presObj.title = data.title;
+                    }
+                    if(data.thumbnail_url) {
+                        presObj.thumbnail_url = data.thumbnail_url;
+                    }
+                    presObj.oembed = data;
                 }
             }
+        }) 
+        }, 500);
+    } else {
+        var newPresentationID = Presentations.insert(presObj);
+        if (newPresentationID) {
+            Router.go("presentations",{_id: newPresentationID});
         }
-    }) 
-    }, 500);
+    }
   },
   'click [data-upload-presentation]': function(event, template) {
       event.preventDefault();
