ac332abc289ee6636fed87a8dad8ff101d47f36c
Michael Elfassy <michael@smashingboxes.com>
2014-11-25 12:08:48 -0500

    correct helper syntax for Meteor1.0

--- client/shared/members/_profilePhotoCircular.js
+++ client/shared/members/_profilePhotoCircular.js
@@ -1,12 +1,14 @@
-Template._profilePhotoCircular.thumbUrl = function(userId) {
-  if(userId) {
-    var user = Meteor.users.findOne({_id:userId});
-    if(user && user.profile.thumbnailUrl !="default-avatar.png") {
-      return user.profile.thumbnailUrl;
+Template._profilePhotoCircular.helpers({
+  thumbUrl: function(userId){
+    if(userId) {
+      var user = Meteor.users.findOne({_id:userId});
+      if(user && user.profile.thumbnailUrl !="default-avatar.png") {
+        return user.profile.thumbnailUrl;
+      } else {
+        return "/default-avatar.png";
+      }
     } else {
       return "/default-avatar.png";
     }
-  } else {
-    return "/default-avatar.png";
   }
-}
+});
